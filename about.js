9231d9d92dbec68be80301095004e44c331c5552
Tommy Clare <tclare@nd.edu>
2018-09-04 16:19:50 -0400

    new and improved

---
+++ about.js
@@ -0,0 +1,37 @@
+(function() {
+
+  $(document).ready(initialize);
+  function initialize() {
+    navbar();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $(window).resize(navbar);
+    $('.collapsible-header').click(switchIcons);
+  }
+
+
+  function navbar() {
+      if ($(window).width() >= 467 && $(window).width() < 837) {
+        $('.navbar').css('height','103px');
+        $('.navbar').css('padding','3px');
+      }
+      else if ($(window).width() < 467) {
+         $('.navbar').css('height','158px');
+         $('.navbar').css('padding','3px');
+      }
+      else if ($(window).width() >= 837) {
+         $('.navbar').css('height','47px');
+         $('.navbar').css('padding','0px');
+      }
+  }
+
+  function switchIcons() {
+    if ($(this).children('i').html() == 'add') {
+      $('.collapsible-header i').html('add');
+      $(this).children('i').html('remove');
+    } else {
+      $(this).children('i').html('add');
+    }
+  }
+
+})();
