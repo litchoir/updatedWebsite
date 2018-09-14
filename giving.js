1ac727d196c4b2a6c009071927fd01e464afbf41
Tommy Clare <tclare@nd.edu>
2018-09-14 15:52:22 -0400

    updated

--- giving.js
+++ giving.js
@@ -9,7 +9,15 @@
   }
 
   function navbar() {
-      if ($(window).width() >= 467 && $(window).width() < 837) {
+
+      console.log("in navbar!")
+      // Navbar and line stuff
+
+      if ($(window).width() >= 837) {
+         $('.navbar').css('height','47px');
+         $('.navbar').css('padding','0px');
+      }
+      else if ($(window).width() >= 467 && $(window).width() < 837) {
         $('.navbar').css('height','103px');
         $('.navbar').css('padding','3px');
       }
@@ -17,10 +25,41 @@
          $('.navbar').css('height','158px');
          $('.navbar').css('padding','3px');
       }
-      else if ($(window).width() >= 837) {
-         $('.navbar').css('height','47px');
-         $('.navbar').css('padding','0px');
+      if ($(window).width() >= 1055) {
+        $('#searchBar').css('margin-top','37px');
+        $('#brandRow').css('height','150px');
+        $('#supportBtn').css('margin-top','57px');
       }
+      else if ($(window).width() >= 568 && $(window).width() < 1055) {
+        $('#brandRow').css('height','200px');
+        $('#searchBar').css('margin-top','60px');
+        $('#supportBtn').css('margin-top','80px');
+      }
+      else if ($(window).width() < 568 && $(window).width() >= 444) {
+        $('#brandRow').css('height','270px');
+        $('#searchBar').css('margin-top','97px');
+      }
+      else if ($(window).width() < 444) {
+        $('#brandRow').css('height','330px');
+      }
+
+      // Button renaming
+
+      if ($(window).width() >= 1211) {
+        $('#supportBtn').html('SUPPORT THE CHOIR');
+        $('#supportBtn').css('visibility','visible');
+        $('#searchBar').css('visibility','visible');
+      }
+      else if ($(window).width() < 1211 && $(window).width() > 717) {
+        $('#supportBtn').html('SUPPORT');
+        $('#supportBtn').css('visibility','visible');
+        $('#searchBar').css('visibility','visible');
+      }
+      else if ($(window).width() < 717) {
+        $('#supportBtn').css('visibility','hidden');
+        $('#searchBar').css('visibility','hidden');
+      }
+
   }
 
 
--- home.js
+++ home.js
@@ -12,14 +12,18 @@
     });
     $(window).resize(slideshow);
     $(window).resize(navbar);
-
   }
 
   function navbar() {
 
+      console.log("in navbar!")
       // Navbar and line stuff
 
-      if ($(window).width() >= 467 && $(window).width() < 837) {
+      if ($(window).width() >= 837) {
+         $('.navbar').css('height','47px');
+         $('.navbar').css('padding','0px');
+      }
+      else if ($(window).width() >= 467 && $(window).width() < 837) {
         $('.navbar').css('height','103px');
         $('.navbar').css('padding','3px');
       }
@@ -27,29 +31,22 @@
          $('.navbar').css('height','158px');
          $('.navbar').css('padding','3px');
       }
-      else if ($(window).width() >= 837) {
-         $('.navbar').css('height','47px');
-         $('.navbar').css('padding','0px');
-      }
-      if ($(window).width() >= 1056) {
+      if ($(window).width() >= 1055) {
         $('#searchBar').css('margin-top','37px');
         $('#brandRow').css('height','150px');
         $('#supportBtn').css('margin-top','57px');
       }
-      else if ($(window).width() > 586 && $(window).width() < 1056) {
+      else if ($(window).width() >= 568 && $(window).width() < 1055) {
         $('#brandRow').css('height','200px');
         $('#searchBar').css('margin-top','60px');
         $('#supportBtn').css('margin-top','80px');
       }
-      else if ($(window).width() < 586 && $(window).width() >= 444) {
+      else if ($(window).width() < 568 && $(window).width() >= 444) {
         $('#brandRow').css('height','270px');
-        $('#searchBar').css('margin-top','80px');
-        $('#supportBtn').css('margin-top','100px');
+        $('#searchBar').css('margin-top','97px');
       }
-      else if ($(window).width() < 444 && $(window).width() >= 444) {
-        $('#brandRow').css('height','340px');
-        $('#searchBar').css('margin-top','100px');
-        $('#supportBtn').css('margin-top','120px');
+      else if ($(window).width() < 444) {
+        $('#brandRow').css('height','330px');
       }
 
       // Button renaming
@@ -57,13 +54,16 @@
       if ($(window).width() >= 1211) {
         $('#supportBtn').html('SUPPORT THE CHOIR');
         $('#supportBtn').css('visibility','visible');
+        $('#searchBar').css('visibility','visible');
       }
       else if ($(window).width() < 1211 && $(window).width() > 717) {
         $('#supportBtn').html('SUPPORT');
         $('#supportBtn').css('visibility','visible');
+        $('#searchBar').css('visibility','visible');
       }
       else if ($(window).width() < 717) {
         $('#supportBtn').css('visibility','hidden');
+        $('#searchBar').css('visibility','hidden');
       }
 
   }
