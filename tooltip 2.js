586c66bd14ce2c0a3bc957b94e9f4fa1b48524c4
tclare <tclare@nd.edu>
2020-05-08 13:33:34 -0400

    most excess pictures deleted

--- tooltip 2.js
+++
@@ -1,5 +0,0 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    var options = {'exitDelay': 3000};
-    var instances = M.Tooltip.init(elems, options);
-  });
